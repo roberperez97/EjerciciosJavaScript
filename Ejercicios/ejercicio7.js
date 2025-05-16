bca09564fce6c0c859f02b3c64e47ededa3e6005
Roberto Pérez <roberto.perez.mesa@gmail.com>
2025-05-16 17:40:28 +0100

    Ejercicio 7 terminado

---
+++ Ejercicios/ejercicio7.js
@@ -0,0 +1,7 @@
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function getMaxNumber(numberOne , numberTwo) {
+  return numberOne > numberTwo ? numberOne : numberTwo;
+}
+
+console.log(getMaxNumber(20,40));
